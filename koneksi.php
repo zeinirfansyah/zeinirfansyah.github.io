007f4c45ccd96aaa6e4162426aba9da30411b95a
Zein Irfansyah <zeinirfansyah@users.noreply.github.com>
2020-12-24 20:52:50 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "us-cdbr-east-02.cleardb.com";
 $username = "b7854a73c3d120";
 $password = "91798983";
-$database = "pemrograman_web";
+$database = "heroku_ca04285c2394a64";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
