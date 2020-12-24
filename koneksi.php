09cc00c67d0e5eb5a57740fe8275329e1c8e1565
Zein Irfansyah <zeinirfansyah@users.noreply.github.com>
2020-12-24 20:36:32 +0700

    Fix typo database name

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "us-cdbr-east-02.cleardb.com";
 $username = "b7854a73c3d120";
 $password = "91798983";
-$database = "Pemrograman_Web";
+$database = "pemrograman_web";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
